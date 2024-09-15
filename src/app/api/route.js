f6e7079da972b922451efa563803bc50e861277c
DADONG HU <hudadong@gmail.com>
2024-09-15 21:51:16 +0800

    change rows

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,50 +1,49 @@
 import { NextResponse } from "next/server";
-// import { BigQuery } from "@google-cloud/bigquery";
+import { BigQuery } from "@google-cloud/bigquery";
 
 
-// const bigquery = new BigQuery(
-//     {
-//       projectId: 'test-app-edf74',
-//     }
-//   );
+const bigquery = new BigQuery(
+  {
+    projectId: 'test-app-edf74',
+  }
+);
 
-//   const bigqueryTable = "test-app-edf74.appTestData.app_test_data_table"
-
+// const bigqueryTable = "test-app-edf74.appTestData.app_test_data_table"
 
 
 export async function GET(req, res) {
-    // const bigquery = new BigQuery();
-    // const query = `SELECT 1`;
-    // const [datasets] = await bigquery.getDatasets({projectId: "my-test-project-361004"});
-    // console.log('Datasets:');
-    // datasets.forEach(dataset => console.log(dataset.id));
+  // const bigquery = new BigQuery();
+  // const query = `SELECT 1`;
+  // const [datasets] = await bigquery.getDatasets({projectId: "my-test-project-361004"});
+  // console.log('Datasets:');
+  // datasets.forEach(dataset => console.log(dataset.id));
 
   //   const projectId = "test-app-edf74"
 
-  // const options = {
-  //     // Specify a job configuration to set optional job resource properties.
-  //     configuration: {
-  //       query: {
-  //         query: `SELECT *
-  //               FROM test-app-edf74.appTestData.app_test_data_table
-  //               LIMIT 100`,
-  //         useLegacySql: false,
-  //       },
-  //     },
-  //   };
+  const options = {
+    // Specify a job configuration to set optional job resource properties.
+    configuration: {
+      query: {
+        query: `SELECT *
+                FROM test-app-edf74.appTestData.app_test_data_table
+                LIMIT 100`,
+        useLegacySql: false,
+      },
+    },
+  };
 
-  //   const response = await bigquery.createJob(options);
-  //   const job = response[0];
+  const response = await bigquery.createJob(options);
+  const job = response[0];
   //   // Wait for the query to finish
-  //   const [rows] = await job.getQueryResults(job)
-    // console.log(rows);
+  const [rows] = await job.getQueryResults(job)
+  // console.log(rows);
 
 
-    const row  ={
-        name: "John Doe",
-        age: 30,
-        email: "john.doe@example.com"
-    }
-    return NextResponse.json(row);
+  // const row  ={
+  //     name: "John Doe",
+  //     age: 30,
+  //     email: "john.doe@example.com"
+  // }
+  return NextResponse.json(rows);
 
 }
