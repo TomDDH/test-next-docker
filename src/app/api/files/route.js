598a17ca64786b52eeb546567cdc28b821156bd6
DADONG HU <hudadong@gmail.com>
2024-09-15 22:41:16 +0800

    add upload post

--- src/app/api/files/route.js
+++ src/app/api/files/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
-
+import { writeFile } from "fs/promises";
 export async function GET(request, res) {
   const directoryPath = path.join(__dirname);
   // const data  =await req.json()
@@ -21,7 +21,33 @@ export async function GET(request, res) {
 
   console.log(files)
 
+  return NextResponse.json({ files, directoryPath });;
 
-  return NextResponse.json({files,directoryPath});;
+}
+
+export async function POST(request, res) {
+  const formData = await request.formData()
+
+  const file = formData.getAll('file')[0]
+  const buffer = Buffer.from(await file.arrayBuffer());
+  const filename = file.name.replaceAll(" ", "_");
+  console.log(buffer)
+  const directoryPath = path.join(__dirname);
+  console.log(directoryPath)
+
+  try {
+    // Write the file to the specified directory (public/assets) with the modified filename
+    await writeFile(
+      path.join('./mnt/', filename),
+      buffer
+    );
+    // Return a JSON response with a success message and a 201 status code
+    return NextResponse.json({ Message: "Success", status: 201 });
+  } catch (error) {
+    // If an error occurs during file writing, log the error and return a JSON response with a failure message and a 500 status code
+    console.log("Error occurred ", error);
+    return NextResponse.json({ Message: "Failed", status: 500 });
+  }
+
 
 }
