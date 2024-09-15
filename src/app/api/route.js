cc1dd9db6ab2fcabc1573685630ff405727483f1
DADONG HU <hudadong@gmail.com>
2024-09-15 20:47:11 +0800

    update router

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
-// import { BigQuery } from "@google-cloud/bigquery";
+import { BigQuery } from "@google-cloud/bigquery";
 
 
-// const bigquery = new BigQuery(
-//     {
-//       projectId: 'test-app-edf74',
-//     }
-//   );
+const bigquery = new BigQuery(
+    {
+      projectId: 'test-app-edf74',
+    }
+  );
 
-//   const bigqueryTable = "test-app-edf74.appTestData.app_test_data_table"
+  const bigqueryTable = "test-app-edf74.appTestData.app_test_data_table"
 
 
 
@@ -19,30 +19,25 @@ export async function GET(req, res) {
     // console.log('Datasets:');
     // datasets.forEach(dataset => console.log(dataset.id));
 
-//     const projectId = "test-app-edf74"
+    const projectId = "test-app-edf74"
 
-//   const options = {
-//       // Specify a job configuration to set optional job resource properties.
-//       configuration: {
-//         query: {
-//           query: `SELECT *
-//                 FROM test-app-edf74.appTestData.app_test_data_table
-//                 LIMIT 100`,
-//           useLegacySql: false,
-//         },
-//       },
-//     };
+  const options = {
+      // Specify a job configuration to set optional job resource properties.
+      configuration: {
+        query: {
+          query: `SELECT *
+                FROM test-app-edf74.appTestData.app_test_data_table
+                LIMIT 100`,
+          useLegacySql: false,
+        },
+      },
+    };
 
-//     const response = await bigquery.createJob(options);
-//     const job = response[0];
-//     // Wait for the query to finish
-//     const [rows] = await job.getQueryResults(job)
+    const response = await bigquery.createJob(options);
+    const job = response[0];
+    // Wait for the query to finish
+    const [rows] = await job.getQueryResults(job)
     // console.log(rows);
-    const data = {
-        "name": "John Doe",
-        "age": 30,
-        "email": "john.doe@example.com"
-    }
-    return NextResponse.json(data);
+    return NextResponse.json(rows);
 
 }
